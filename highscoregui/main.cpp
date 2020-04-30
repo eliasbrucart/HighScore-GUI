bc8bf3040ef1117ad46f094203235c8d532eb590
Elias <eliasbrucart@hardgames.com.ar>
2020-04-29 22:24:09 -0300

    Added FLTK library and run first program

--- highscoregui/main.cpp
+++ highscoregui/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include "Highscore.h"
+#include "FL/Fl.H"
+#include "FL/Fl_Window.H"
+#include "FL/Fl_Box.H"
 
 using namespace std;
 
 using namespace highscore;
 
-Score list[10] = { 0,0,0,0,0,0,0,0,0,0 };
+//Score list[10] = { 0,0,0,0,0,0,0,0,0,0 };
 
-void main()
-{
-	InitList(list);
+int main(int argc, char **argv) {
+	Fl_Window *window = new Fl_Window(340, 180);
+	Fl_Box *box = new Fl_Box(20, 40, 300, 100, "Para vos Fer");
+	box->box(FL_UP_BOX);
+	box->labelfont(FL_BOLD + FL_ITALIC);
+	box->labelsize(36);
+	box->labeltype(FL_SHADOW_LABEL);
+	window->end();
+	window->show(argc, argv);
+	return Fl::run();
 }
