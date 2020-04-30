9e9d7c921434b16fdec1ef48c18b081d6d4f7fd9
Elias <eliasbrucart@hardgames.com.ar>
2020-04-30 00:37:26 -0300

    Added configuration for Release version

--- highscoregui/main.cpp
+++ highscoregui/main.cpp
@@ -12,7 +12,7 @@ using namespace highscore;
 
 int main(int argc, char **argv) {
 	Fl_Window *window = new Fl_Window(340, 180);
-	Fl_Box *box = new Fl_Box(20, 40, 300, 100, "Para vos Fer");
+	Fl_Box *box = new Fl_Box(20, 40, 300, 100, "Hello World");
 	box->box(FL_UP_BOX);
 	box->labelfont(FL_BOLD + FL_ITALIC);
 	box->labelsize(36);
